0a6925aa6aeb7d36408451367e8f56531ba743e0
Nirajan Thapa <nirajan.thapa@wework.com>
2019-11-20 02:01:56 -0500

    Toggle views visibility

--- app/src/main/java/com/twitter/challenge/weather/WeatherFragment.kt
+++ app/src/main/java/com/twitter/challenge/weather/WeatherFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.twitter.challenge.R
 import com.twitter.challenge.base.BaseFragment
@@ -13,6 +16,8 @@ import dagger.android.support.AndroidSupportInjection
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_weather.data_container
+import kotlinx.android.synthetic.main.fragment_weather.progressbar
 import kotlinx.android.synthetic.main.fragment_weather.temperature
 import kotlinx.android.synthetic.main.fragment_weather.wind
 
@@ -30,29 +35,36 @@ class WeatherFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onStart() {
-        super.onStart()
         // Subscribe to the emissions of the weather data from the view model.
         // Update the weather info at every onNext emission.
         // In case of error, log the exception.
         disposable.add(weatherViewModel.getCurrentWeather("current.json")
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                renderWeatherData(it)
-            },
+            .doFinally { toggleProgressBar(show = false) }
+            .subscribe(
+                {
+                    renderWeatherData(it)
+                },
                 { error ->
-                    Log.e(TAG, "Unable to get username", error)
+                    Log.e(TAG, "Unable to get weather", error)
                 }
-            ))
+            )
+        )
     }
 
     private fun renderWeatherData(weatherModel: WeatherModel) {
         val currTmp: Float = weatherModel.weather.temp.toFloat()
         temperature.text = getString(R.string.temperature, currTmp, TemperatureConverter.celsiusToFahrenheit(currTmp))
         wind.text = weatherModel.wind.speed.toString()
+        data_container.visibility = VISIBLE
+    }
+
+    private fun toggleProgressBar(show: Boolean) {
+        if (show && !progressbar.isVisible)
+            progressbar.visibility = VISIBLE
+        else if (!show && progressbar.isVisible)
+            progressbar.visibility = GONE
     }
 
     override fun onStop() {
